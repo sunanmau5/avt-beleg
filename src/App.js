09a6c6b2f2d617299ed7c85f9c6441682b75da12
sunanmau5 <sunan.regi@gmail.com>
2021-07-10 13:11:50 +0200

    feat: add multiple targets react dnd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Container } from './components/MultipleTargets/Container';
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider } from 'react-dnd'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className="App">
+        <Container></Container>
+      </div>
+    </DndProvider>
   );
 }
 
